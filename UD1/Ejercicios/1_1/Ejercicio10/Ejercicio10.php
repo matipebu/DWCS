3fb7ccf88824916de5731b70fc94ba00e3598d82
matipebu <matipebu@hotmail.com>
2025-09-24 11:54:58 +0200

    Ejercicio10

--- UD1/Ejercicios/1_1/Ejercicio10/Ejercicio10.php
+++ UD1/Ejercicios/1_1/Ejercicio10/Ejercicio10.php
@@ -1,7 +1,7 @@
 <?php
     function generarNumeros(int $nivel){
             $numeros=[];
-            for($i = 0; $i < nivel; $i++){
+            for($i = 0; $i < $nivel; $i++){
                 $numeros[]=rand(1,4);
             }
             return $numeros;
@@ -17,6 +17,12 @@
 
     }
 
+    $numsIn = $_POST['in_nums']??[];
+    $nums = $_POST['nums']??[];
+    $nivel = $_POST['nivel']??0;
+
+     $jugando = (comprobarNumeros(explode("-",$numsIn),explode("-",$nums)));
+       
 
 
 ?>
@@ -27,20 +33,57 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SIMON DICE</title>
+    <style>
+        .hidden{
+            display: none;
+        }
+    </style>
+    <script>
+        function ocultarNumero(){
+            setTimeout(
+                function(){
+                    document.getElementById('numeros').classList.add('hidden');
+                    document.getElementById('formulario').classList.remove('hidden');
+                 
+                }
+                ,3000
+            );
+        }
+    </script>
 
 </head>
-<body>
-    <form>
-        <label for="numero">Ingresa un número: </label>
-        <input type="text" id="numero" name="numero" required>
-        <button type="submit">Comprobar</button>
-    </form>
+<body onload="ocultarNumero()">
+    <h1>Simon Dice</h1>
+    <?php if ($jugando): ?>
+    <div id="numeros">
+        <h2>Memoriza los numeros</h2>
+        <?php
+            $nivel++;
+            $nums = generarNumeros($nivel);
+            echo implode("-",$nums);
+        ?>
+    
+    </div>
+    <div id="formulario" class="hidden">
+        <form action="" method="post">
+            <label for="in_nums">Números </label><br>
+            <input type="text" name="in_nums" value="<?php echo implode (".",$nums)?>"></input><br>
+            <input type="text" name="nivel" value="<?php echo $nivel;?>">
+            <button type="submit">Comprobar</button>
+        </form>
+    </div>
+    <?php else:?>
+        <div id="resultado">
+            Has perdido
+            <?php endif; ?>
+        </div>
+    
+ 
     <?php
     $goal = [2,3,4];
     $target = [2,3,4];
     echo"</br>";
     echo 'Los numeros son '.(comprobarNumeros($target, $goal) ? 'Iguales' : 'Diferentes');
-    
     ?>
       
 </body>
