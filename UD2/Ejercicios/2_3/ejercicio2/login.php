72ef97744198f600e116af96b1a6c63d855d93ac
matipebu <matipebu@hotmail.com>
2025-10-23 14:28:55 +0200

    ejercicios 2_3 2

--- UD2/Ejercicios/2_3/ejercicio2/login.php
+++ UD2/Ejercicios/2_3/ejercicio2/login.php
@@ -1,3 +1,30 @@
+<?php
+require_once "funciones.php";
+session_start();
+$nic = $_POST['nic'];
+$pass = $_POST['pass'];
+    
+
+if(isset($_POST['rec'])){
+    setcookie('nic',$nic,time()+2592000);
+
+}
+
+if($_SERVER['REQUEST_METHOD']=='POST'){
+   
+    if(comprobar_usuario($nic,$pass)){
+        $_SESSION['nic'] = $nic;
+        header("Location: restringido.php");
+        exit();
+    }else{
+        echo "Tienes que registrarte primero";
+
+    }
+    
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -12,7 +39,8 @@
             <input type="text" name="nic"><br>
             <label for="pass">Contraseña</label><br>
             <input type="password" name="pass"><br>
-            <button type="submit">Acceder</button>
+            <button type="submit">Acceder</button><br>
+            <input type="checkbox" name="rec" id="rec">Recordar</input>
         </form>
     </fieldset>
     <a href="registro.php">Registrar usuario</a>
--- UD2/Ejercicios/2_3/ejercicio2/restringido.php
+++ UD2/Ejercicios/2_3/ejercicio2/restringido.php
@@ -1,15 +1,48 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['nic'])) {
+    header("Location: login.php");
+    exit();
+}
+if (isset($_GET['logout'])) {
+    unset($_SESSION['nic']);
+    session_unset();
+    $cookie_params = session_get_cookie_params();
+
+    setcookie(
+        session_name(),
+        '',
+        time() - 3600,
+        $cookie_params['path'],
+        $cookie_params['domain'],
+        $cookie_params['secure'],
+        $cookie_params['httponly']
+    );
+    //Cerramos la sesion
+    session_destroy();
+
+    header("Location: login.php");
+    exit();
+}
+
+
+?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sección restringida</title>
 </head>
+
 <body>
     <h1>Sección restringida</h1>
-    Estás logueado con el usuario [nombre_usuario]. Pulsa aquí para salir: <a href="">Logout</a>
+    Estás logueado con el usuario <?= $_SESSION['nic'] ?>. Pulsa aquí para salir: <a href="?logout">Logout</a>
     <p>
         Esta sección esta restringida solo para los usuarios que están registrados.
     </p>
 </body>
+
 </html>
