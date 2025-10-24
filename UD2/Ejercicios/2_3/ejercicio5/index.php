3faa4c7dd1b02c28daa396910506911891bbf3ce
matipebu <matipebu@hotmail.com>
2025-10-24 13:31:18 +0200

    empezar ejercicio5

---
+++ UD2/Ejercicios/2_3/ejercicio5/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Productos</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <td>Producto</td>
+            <td>Precio</td>
+            <td>Acciones</td>
+            
+        </tr>
+        <tr>
+            <td></td>
+        </tr>
+    </table>
+    
+</body>
+</html>
